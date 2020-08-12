baf11d1d543bcaff150c2b9bc0410f7a73ff6878
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-08-12 09:53:47 -0700

    Finish count negatives

---
+++ countNegatives.js
@@ -0,0 +1,52 @@
+// Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise. 
+
+// Return the number of negative numbers in grid.
+
+ 
+
+// Example 1:
+
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+// Example 2:
+
+// Input: grid = [[3,2],[1,0]]
+// Output: 0
+// Example 3:
+
+// Input: grid = [[1,-1],[-1,-1]]
+// Output: 3
+// Example 4:
+
+// Input: grid = [[-1]]
+// Output: 1
+ 
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 100
+// -100 <= grid[i][j] <= 100
+
+var countNegatives = function(grid) {
+  let count = 0;
+  
+  for(let i = 0; i < grid.length; i += 1){
+    for (let j = 0; j < grid[i].length; j += 1){
+      if(grid[i][j] < 0){
+        count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+
+// let grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]];//8
+// let grid = [[3,2],[1,0]];//0
+// let grid = [[1,-1],[-1,-1]];//3
+let grid = [[-1]];//1
+
+console.log(countNegatives(grid));
