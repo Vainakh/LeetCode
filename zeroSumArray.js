e6b26b2cf3b59c92c93911744a2f0b54daa6184a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-08 07:39:53 -0800

    Add test case

--- zeroSumArray.js
+++ zeroSumArray.js
@@ -17,6 +17,7 @@ const zeroSum = (array) => {
     return newArray;
 }
 
-let array = [-4, -3, -2, -1, 0, 1, 2, 3, 10];
+// let array = [-4, -3, -2, -1, 0, 1, 2, 3, 10];
+let array = [-4, -3, -2, -1, 0, 5, 10];
 
 console.log(zeroSum(array));
