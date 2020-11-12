b35a8cb1ef37b9acf5e234371d2db89d443ce7b7
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-12 09:05:01 -0800

    Add classes to BinarySearchTree

---
+++ BST.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.right = null;
+    this.left = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
