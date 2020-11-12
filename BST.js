2b71611b0f766a728bd11a3bfc82a7714d25ee95
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-12 10:36:54 -0800

    Construct tree root and branches

--- BST.js
+++ BST.js
@@ -11,3 +11,14 @@ class BinarySearchTree {
     this.root = null;
   }
 }
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+
+console.log(tree);
+console.log(tree.root);
+console.log(tree.root.right);
+console.log(tree.root.left);
