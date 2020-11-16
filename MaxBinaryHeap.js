29630aaa7fd8c9b0814e5b24c2bfb4cb2f12d1ca
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-16 08:06:04 -0800

    Add extractMac method to MaxHeap

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -18,6 +18,44 @@ class MaxBinaryHeap {
         idx = parentIdx;
     }
   }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element= this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChild < length) {
+        leftChild = this.values(leftChildIdx);
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChild < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)){
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 //[41, 39, 33, 18, 27, 12]
@@ -29,4 +67,5 @@ heap.insert(1);
 heap.insert(45);
 heap.insert(199);
 
+console.log(heap.extractMax());
 console.log(heap);
