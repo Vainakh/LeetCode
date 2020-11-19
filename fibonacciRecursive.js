c01a05d628ef780c8ac9f67b2b6689746a5264dd
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 12:27:39 -0800

    Finish memoized solution for fibonacci num function

--- fibonacciRecursive.js
+++ fibonacciRecursive.js
@@ -1,8 +1,21 @@
-const fibNum = (num) => {
+//naive solution
+
+const fibNumRecursive = (num) => {
   if (num <= 2) return 1;
-  return fibNum(num - 1) + fibNum(num - 2);
+  return fibNumRecursive(num - 1) + fibNumRecursive(num - 2);
 }
 
-let num = 7;
 
-console.log(fibNum(num));
+//memoized solution 
+
+const fibNumMemoized = (num, memo = []) => {
+  if (memo[n] !== undefined) return memo[n];
+  if (num <= 2) return 1;
+  let res = fibNumMemoized(n - 1, memo) + fibNumMemoized(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+
+let num = 7;
+console.log(fibNumRecursive(num));
