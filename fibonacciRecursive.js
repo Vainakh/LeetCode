a74156484ca6dcbb26ab18ef296d375e06f87ea7
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 12:10:27 -0800

    Fix base case

--- fibonacciRecursive.js
+++ fibonacciRecursive.js
@@ -1,8 +1,8 @@
 const fibNum = (num) => {
-  if (num < 2) return 1;
+  if (num <= 2) return 1;
   return fibNum(num - 1) + fibNum(num - 2);
 }
 
-let num = 4;
+let num = 7;
 
 console.log(fibNum(num));
