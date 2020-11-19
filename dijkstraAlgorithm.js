39162316c45b6795ca54b6ec84eb57d3076742bf
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 07:08:20 -0800

    Add test cases

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -14,13 +14,6 @@ class PriorityQueue {
   }
 }
 
-
-
-
-
-
-
-
 class WeightedGraph {
   constructor() {
     this.adjancencyList = {};
@@ -39,9 +32,17 @@ let graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-graph.addEdge("A", "B", 9);
-graph.addEdge("A", "C", 5);
-graph.addEdge("B", "C", 7);
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
 
 console.log(graph);
