60911c1ecea8a6d48e120a5f6611359b1fd55b52
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 11:00:49 -0800

    Finish Dijktra's Algorithm

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -25,6 +25,56 @@ class WeightedGraph {
     this.adjancencyList[vertex1].push({node: vertex2, weight});
     this.adjancencyList[vertex2].push({node: vertex1, weight});
   }
+  dijkstraAlgo(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = []; //to return at the end
+    let smallest;
+
+    //build up initial state
+    for (let vertex in this.adjancencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    //as long as there is something to visit
+    while(nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === finish) {
+        //we are done
+        //build up path to return to end
+        while(previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjancencyList[smallest]) {
+          //find neighboring node
+          let nextNode = this.adjancencyList[smallest][neighbor];
+          //calculate new distance to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            //updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updating previous - how we got to neighbor
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
 }
 
 let graph = new WeightedGraph();
@@ -44,5 +94,7 @@ graph.addEdge("C", "F", 4);
 graph.addEdge("D", "E", 3);
 graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
+
+graph.dijkstraAlgo("A", "E");
 
 console.log(graph);
