9f197fce8f30408ea8d761a78c10470601a6d383
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 05:58:20 -0800

    Add add vertex method on Weighted Graph

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -2,4 +2,7 @@ class WeightedGraph {
   constructor() {
     this.adjancencyList = {};
   }
+  addVertex(vertex) {
+    if (!this.adjancencyList[vertex]) return this.adjancencyList[vertex] = []; 
+  }
 }
