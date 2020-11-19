204e875ecf29a76d7de62be7ae5c32130aca2ebe
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-19 06:01:35 -0800

    Add add edge method to Weighted Graph

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -5,4 +5,8 @@ class WeightedGraph {
   addVertex(vertex) {
     if (!this.adjancencyList[vertex]) return this.adjancencyList[vertex] = []; 
   }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjancencyList[vertex1].push({node: vertex2, weight});
+    this.adjancencyList[vertex2].push({node: vertex1, weight});
+  }
 }
