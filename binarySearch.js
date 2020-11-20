a9b22122de50e97c78bbc8e380636b80c0171583
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-20 12:08:51 -0800

    Refacture binary search function

--- binarySearch.js
+++ binarySearch.js
@@ -1,25 +1,19 @@
+
+
 const binarySearch = (array, value) => {
   let sorted = array.sort((a, b) => a - b);
   let start = 0;
   let end = sorted.length - 1;
-
-  while(start < end){
-    let mid = Math.floor((start + end) / 2);
-
-    if(sorted[mid] === value){
-      return mid;
-    }
-
-    if(sorted[mid] < value){
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
+  let middle = Math.floor((start + end) / 2);
+  while (value !== sorted[middle] && start <= end) { 
+    if (value < sorted[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
   }
-  return -1;
-};
+  return sorted[middle] === value ? middle : -1;
+}
 
-let array = [2, 4, 0, 5, 9, 6, 12, 54, 34, 18, 7];
-let value = 7;
+let array = [2, 5, 6, 9, 13, 15, 28, 30];
+let value = 28;
 
 console.log(binarySearch(array, value));
