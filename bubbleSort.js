ef81686f1577075e0a0fd3fabd4767e46b374537
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-21 05:16:13 -0800

    Bubble sort es6 solution

--- bubbleSort.js
+++ bubbleSort.js
@@ -11,6 +11,21 @@ const bubbleSort = (array) => {
   return array;
 }
 
+// const bubbleSort = (array) => {
+//   const swap = (array, idx1, idx2) => {
+//     [array[idx1][idx2]] = [array[idx2][idx1]];
+//   }
+
+//   for (let i = array.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         swap(array, j, j + 1)
+//       }
+//     }
+//   }
+//   return array;
+// }
+
 let array = [2, 15, 9, 10, 1, 11, 23, -2, 3];
 
 console.log(bubbleSort(array));
