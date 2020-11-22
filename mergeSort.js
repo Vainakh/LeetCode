3e3ed75a2d40c6637f0837c4dbc1b0045aea076d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-22 05:25:54 -0800

    Add mergeSort function

--- mergeSort.js
+++ mergeSort.js
@@ -23,7 +23,17 @@ const merge = (array1, array2) => {
   return results;
 }
 
-let array1 = [1, 3, 9, 10, 62, 100];
-let array2 = [1, 2, 3, 5, 6];
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+  let mid = Math.floor(array.length /  2);
+  let left = mergeSort(array.slice(0, mid));
+  let right = mergeSort(array.slice(mid));
+  return merge(left, right);
+}
 
-console.log(merge(array1, array2))
+// let array1 = [1, 3, 9, 10, 62, 100];
+// let array2 = [1, 2, 3, 5, 6];
+let array = [10, 24, 76, 73, 72, 1, 9];
+
+// console.log(merge(array1, array2));
+console.log(mergeSort(array));
