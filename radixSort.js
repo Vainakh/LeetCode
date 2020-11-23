ea2e0937275a7983935df03be332640a257c68ed
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-23 07:15:55 -0800

    Add most digit method

--- radixSort.js
+++ radixSort.js
@@ -6,3 +6,14 @@ const countDigit = (num) => {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
+
+const mostDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, countDigit(nums[i]));
+  }
+  return maxDigits;
+}
+
+let nums = [12, 234, 54322, 12, 988, 299];
+console.log(mostDigits(nums));
