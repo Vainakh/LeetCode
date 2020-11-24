4378a78d577ef582690b688828a46309662c95b1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-24 05:15:28 -0800

    Update validAnagram

--- validAnagram.js
+++ validAnagram.js
@@ -3,36 +3,28 @@
 function validAnagram(string1, string2){
   let first = {};
   let second = {};
-// add whatever parameters you deem necessary - good luck!
-  if (string1.length !== string2.length) {
-      return false;
-  }
 
   for (let i = 0; i < string1.length; i++) {
-    first[string1[i]] = (first[string1[i]] || 0) + 1; 
+      first[string1[i]] = (first[string1[i]] || 0 ) + 1;
   }
-
-  for (let y = 0; y < string2.length; y++) {
-    second[string2[y]] = (second[string2[y]] || 0) + 1;
+  for (let j = 0; j < string2.length; j++) {
+    first[string2[j]] = (first[string2[j]] || 0 ) + 1;
   }
-
-  for (let key in first) {
+  for (let key in string1) {
     if (!key in second) {
       return false;
     }
-
-    if (first[key] !== second[key]) {
+    if(string1[key] !== string2[key]) {
       return false;
     }
   }
-
   return true;
 }
 
-let string1 = "zaa";//true
-let string2 = "aza";
-
-// let string1 = "zaz";//false
+// let string1 = "zaa";//true
 // let string2 = "aza";
+
+let string1 = "zaz";//false
+let string2 = "aza";
 
 console.log(validAnagram(string1, string2));
