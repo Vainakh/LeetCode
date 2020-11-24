aa4bb34e0792b51ec072869b7159671751e2f046
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-24 06:02:32 -0800

    Add alternative solution to sameFrequency

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -4,15 +4,14 @@ const countUniqueValues = (array) => {
   if (array.length === 0) {
     return [];
   }
-    let i = 0;
-    for (let j = 1; j < array.length; j++) {
-      if (array[i] !== array[j]) {
-        i++;
-        array[i] = array[j];
-      } 
-    }
-
-    return i + 1;
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+    } 
+  }
+  return i + 1;
 };
 
 // let array = [1, 1, 1, 1, 1, 2];//2
---
+++ sameFrequency.js
@@ -0,0 +1,44 @@
+const sameFrequency = (num1, num2) => {
+  let first = ("" + num1).split("").sort();
+  let second = ("" + num2).split("").sort();
+  if(first.length !== second.length) return false;
+  for (let i = 0; i < first.length; i ++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// let num1 = 182;//true
+// let num2 = 281;
+
+// let num1 = 34;//false
+// let num2 = 14;
+
+// let num1 = 3589578;//true
+// let num2 = 5879385;
+
+// let num1 = 22;//false
+// let num2 = 222;
+
+console.log(sameFrequency(num1, num2))
+
+// let first = ("" + num1).split("").sort();
+//     console.log(first)
+//     let second = ("" + num2).split("").sort();
+//     console.log(second)
+
+//     if (first.length !== second.length) {
+//         return false;
+//     }
+
+//     for (let i = 0; i < first.length; i += 1) {
+//         for (let j = 0; j < second.length; j += 1) {
+//                 if (first[i] !== second[j] && i === j) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
