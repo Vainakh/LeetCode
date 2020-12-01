de5b394d08f6df628b06369a33fa10bda959f590
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-30 19:10:15 -0800

    Add alternative provided solution

--- reverseRecursive.js
+++ reverseRecursive.js
@@ -1,3 +1,10 @@
+//provided solution
+
+// function reverse(str){
+// 	if(str.length <= 1) return str;
+// 	return reverse(str.slice(1)) + str[0];
+// }
+
 const reverseRecursive = (string) => {
   if (string.length === 0) return "";
   return  string.slice(-1) + reverseRecursive(string.slice(0, -1));
