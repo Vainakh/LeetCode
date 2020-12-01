90d258875c1e48f8d1ca4c217931fc4e5e3aa4eb
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-11-30 19:14:08 -0800

    Finish alternative solution

--- flattenRecursive.js
+++ flattenRecursive.js
@@ -1,5 +1,20 @@
 //Write a recursive function which accepts an array and return a new array with all values flatten.
 
+//alterantive provided solution
+
+// function flatten(oldArr){
+//   var newArr = []
+//   	for(var i = 0; i < oldArr.length; i++){
+//     	if(Array.isArray(oldArr[i])){
+//       		newArr = newArr.concat(flatten(oldArr[i]))
+//     	} else {
+//       		newArr.push(oldArr[i])
+//     	}
+//   } 
+//   return newArr;
+// }
+
+
 const flatten = (array) => {
   if (array.every((el) => !Array.isArray(el))) return array;
   return flatten(array.reduce((array, el) => {
