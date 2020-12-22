49d42148d61bd251e1bf393d62017f0df7409be1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-21 17:45:10 -0800

    Pluralized nouns

--- capitalizeFirstRecursive.js
+++ capitalizeFirstRecursive.js
@@ -1,8 +1,14 @@
 //Write a recursive function which given an array of strings, capitalizes first letter of each string.
 
-const capitalizeFirst = (array) => {
+const capitalizeFirstHelper = (array) => {
   if (array.length === 0) return [];
   return array.map((el) => el[0].toUpperCase() + el.slice(1));
 } 
+
+const capitalizeFirst = (array, i) => {
+  if (array.length === i) return;
+  array[i] = capitilizeFirstHelper(array[0]);
+  return capitalizeFirst(array, i + 1);
+}
 
 console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
