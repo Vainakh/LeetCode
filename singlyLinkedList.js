599fd0aa4ec1d8de338d62ca166874de123b0f85
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-25 10:10:59 -0800

    Add get method to singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -257,6 +257,16 @@ class SinglyLinkedList {
       this.length++;
       return this;
     }
+    get() {
+      if (index < 0 || index >= this.length) return null;
+      let counter = 0;
+      let current = this.head;
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    }
 };
 
 let list = new SinglyLinkedList();
