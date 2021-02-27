b1de95949cb5330fb0252fce146f39981536e64d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-27 15:15:16 -0800

    Finish compute_sum_of_all_elements

--- hackreactorprep.js
+++ hackreactorprep.js
@@ -238,21 +238,52 @@
 // var output = getLargestElement([5, 2, 8, 3]);
 // console.log(output); // --> 8;
 
-function getLargestElement(arr) {
-  let largest = null;
-  if (arr.length === 0) {
-    return 0;
-  }
-  for (let i = 0; i < arr.length; i ++) {
-    if (largest === null) {
-      largest = arr[i];
-    } else if (largest < arr[i]) {
-      largest = arr[i];
-    }
-  }
+// function getLargestElement(arr) {
+//   let largest = null;
+//   if (arr.length === 0) {
+//     return 0;
+//   }
+//   for (let i = 0; i < arr.length; i ++) {
+//     if (largest === null) {
+//       largest = arr[i];
+//     } else if (largest < arr[i]) {
+//       largest = arr[i];
+//     }
+//   }
 
-  return largest;
-}
+//   return largest;
+// }
 
-let output = [5, 2, 8, 3];
-console.log(getLargestElement(output));
+// let output = [5, 2, 8, 3];
+// console.log(getLargestElement(output));
+
+
+// Write a function called "computeSumOfAllElements".
+
+// Given an array of numbers, "computeSumOfAllElements" returns the sum of all the elements in the given array.
+
+// var output = computeSumOfAllElements([1, 2, 3])
+// console.log(output); // --> 6
+
+// function computeSumOfAllElements(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i ++) {
+//     sum += arr[i];
+//   }
+
+//   return sum;
+// }
+
+// let output = [1, 2, 3];
+// console.log(computeSumOfAllElements(output));
+
+Write a function called "calculateBillTotal".
+
+Given the pre tax and pre tip amount of a meal, "calculateBillTotal" returns the total amount due after tax and tip.
+
+Notes:
+
+Assume that sales tax is 9.5% and tip is 15%.
+Do NOT tip on the sales tax, only on the pre tip amount.
+var output = calculateBillTotal(20);
+console.log(output); // --> 24.9
