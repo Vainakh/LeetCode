23f8c5613e1c330cfd60af48a402d2883a809910
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-28 06:17:15 -0800

    Finish sum digits

--- hackreactorprep.js
+++ hackreactorprep.js
@@ -354,21 +354,56 @@
 // var output = getProductOfAllElementsAtProperty(obj, 'key');
 // console.log(output); // --> 24
 
-function getProductOfAllElementsAtProperty(obj, key) {
-  if (!Array.isArray(obj[key]) || !obj[key] || !obj[key].length) {
-      return 0;
-      }
+// function getProductOfAllElementsAtProperty(obj, key) {
+//   if (!Array.isArray(obj[key]) || !obj[key] || !obj[key].length) {
+//       return 0;
+//       }
       
-  var product = 1;
-  for (var i = 0; i < obj[key].length; i++) {
-      product *= obj[key][i];
+//   var product = 1;
+//   for (var i = 0; i < obj[key].length; i++) {
+//       product *= obj[key][i];
+//   }
+//   return product;
+// }
+
+// var obj = {
+//     key: [1, 2, 3, 4]
+//   };
+//   var output = getProductOfAllElementsAtProperty(obj, 'key');
+//   console.log(output);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a function called "sumDigits".
+
+// Given a number, "sumDigits" returns the sum of all its digits.
+
+// var output = sumDigits(1148);
+// console.log(output); // --> 14
+// If the number is negative, the first digit should count as negative.
+
+// var output = sumDigits(-316);
+// console.log(output); // --> 4
+// Notes:
+
+// In order to use some of the methods that will be most helpful to you, you will most likely want to do some string to number conversion and vice versa.
+// Be sure to familiarize yourself with the "toString" method, as well as the "Number" function.
+
+function sumDigits(num) {
+  let sum = 0;
+  let negative = num < 0 ? true : false;
+  num = negative ? Math.abs(num) : num;
+  while (num) {
+    if (negative && num <= 10) {
+      sum -= num % 10;
+    } else {
+      sum += num % 10;
+    }
+    num = Math.floor(num / 10);
   }
-  return product;
+  return sum;
 }
 
-var obj = {
-    key: [1, 2, 3, 4]
-  };
-  var output = getProductOfAllElementsAtProperty(obj, 'key');
-  console.log(output);
-
+var output = sumDigits(-316);
+console.log(output);
