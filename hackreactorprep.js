74a8873b2a66445517eb540cf19bc363d0110ca1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-28 17:49:26 -0800

    Finish modulo function without the use of native modulo operator

--- hackreactorprep.js
+++ hackreactorprep.js
@@ -922,3 +922,37 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// Write a function called "modulo".
+
+// Given 2 numbers, "modulo" returns the remainder after dividing num1 by num2.
+
+// It should behave as described in the canonical documentation (MDN) for the JavaScript remainder operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder_() 
+
+// Notes:
+
+// Do NOT use the actual built-in modulo (aka "remainder") operator (%) in your implementation.
+// 0 % ANYNUMBER = 0.
+// ANYNUMBER % 0 = NaN.
+// If either operand is NaN, then the result is NaN.
+// Modulo always returns the sign of the first number, even if the remainder is 0.
+// var output = modulo(25, 4);
+// console.log(output); // --> 1
+
+// function modulo(num1, num2) {
+//   if (num1 === 0) return 0;
+//   if (num2 === 0) return NaN;
+//   if (num1 === NaN || num2 === NaN) return NaN;
+//   let number1 = Math.abs(num1);
+//   let number2 = Math.abs(num2);
+//   let mod = number1 - Math.floor(number1/number2) * number2;
+//   if (num1 < 0) return -(mod);
+//   else return mod;
+// };
+
+// let output = modulo(-25, 2);
+// console.log(output); 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
