18f2fd43f1d2687c09e64ebbbfb38047fd852807
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-02 06:00:38 -0800

    Finish detect outlier value

--- hackreactorprep.js
+++ hackreactorprep.js
@@ -1192,7 +1192,38 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// Given a string of even and odd numbers, find which is the sole even number or the sole odd number.
 
+// The return value should be 1-indexed, not 0-indexed.
+
+// Example:
+
+// detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
+// detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the re
+
+function detectOutlierValue(string) {
+  let arr = string.split(' ');
+  let even = [];
+  let odd = [];
+  for (let i = 0; i < arr.length; i ++) {
+    let current = Number(arr[i]);
+    if (current % 2 === 0) {
+      odd.push(i + 1);
+    } else if (current % 2 !== 0){
+      even.push(i + 1);
+    }
+  }
+  if (even.length === 1) {
+    let stringed =  even.join('');
+    return parseInt(stringed);
+  } else {
+    let stringed = odd.join('');
+    return parseInt(stringed);
+  }
+}
+
+console.log(detectOutlierValue("2 4 7 8 10")); 
+console.log(detectOutlierValue("1 10 1 1"));
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
