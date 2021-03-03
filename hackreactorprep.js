426d4b2489eef2eb3e95cd1f97f6d92d8fef73fb
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-03 06:53:28 -0800

    Finish binary search

--- hackreactorprep.js
+++ hackreactorprep.js
@@ -1289,3 +1289,47 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// Binary search is a technique for very rapidly searching a sorted collection by cutting the search space in half at every pass.
+
+// Given a sorted array, such as this: [1, 3, 16, 22, 31, 33, 34]
+
+// You can search for the value 31, as follows:
+
+// 1) Pick the midpoint: 22. 2) The value is higher than 22, so now consider only the right half of the previous array: [...31, 33, 34] 3) Pick the midpoint: 33 4) The value is lower than 33, so now consider only the left half of the previous array: [...31...] 5) Pick the midpoint: 31 6) You've hit the value. 7) Return the index of the value.
+
+// Notes:
+
+// If you don't find the value, you can return null.
+// If at any point you calculate the index of the midpoint and get a fractional number, just round it down ("floor" it).
+// Example:
+
+// var arr = [1, 3, 16, 22, 31, 33, 34]
+// var result = search(arr, 31);
+// console.log(result); //--> 4
+
+
+// function search(array, value) {
+//   let min = 0;
+//   let max = array.length - 1;
+//   let mid;
+
+//   while( min <= max) {
+//     mid = Math.floor((min + max) / 2);
+//     if (array[mid] === value) {
+//       return mid;
+//     } else if (array[mid] < value) {
+//       min = mid + 1;
+//     } else {
+//       max = mid -1;
+//     }
+//   }
+
+//   return -1;
+// };
+
+// var arr = [1, 3, 16, 22, 31, 33, 34]
+// var result = search(arr, 31);
+// console.log(result);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
