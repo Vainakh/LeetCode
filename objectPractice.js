bc3d4cf689a56c4d3684c6b4847acb0687eb75dc
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-08 05:46:23 -0800

    Finish solution sliding window

--- objectPractice.js
+++ objectPractice.js
@@ -1041,3 +1041,58 @@
 // assertArraysEqual(actual_1, expected_1, "creates the correct sample view")
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Comma Separated Integers
+// You are going to create a function called "solution", which will take in an array of increasing integers, and return them in the format described below.
+
+// A format for expressing an ordered list of integers is to use a comma separated list of either individual integers or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'.
+
+// The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers.
+
+// For example, an input of [12, 13, 15, 16, 17] would return "12, 13, 15-17"
+
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format. Below is an example of your code running, assuming you have solved the problem correctly.
+
+// solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// // returns "-6,-3-1,3-5,7-11,14,15,17-20"
+
+// solution([-4, -3, -2, -1, 2, 3, 5, 6, 12, 13, 14, 15, 17]);
+// // returns "-4--1,2,3,5,6,12-15,17"
+
+
+// function solution(integers) {
+//   if (integers.length > 0 && integers.length < 3) {
+//     return integers.join(',');
+//   };
+//   let result = [];
+//   let start = 0;
+//   let next = 1;
+//   let end = 2;
+
+//   while(start < integers.length) {
+//     if (integers[end] - integers[next] === 1 && integers[next] - integers[start] === 1) {
+//       while (integers[end] - integers[next] === 1) {
+//         next = end;
+//         end = end + 1;
+//       } 
+//       let sequence = integers.slice(start, end);
+//       result.push(`${sequence[0]}-${sequence[sequence.length - 1]}`);
+//       start = end;
+//       next = end + 1;
+//       end = end + 2;
+//     } else {
+//       result.push(integers[start]);
+//       start = next;
+//       next = end;
+//       end = end + 1;
+//     }
+//   }
+
+//   return result.join(',');
+// };
+
+// let input = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// console.log(input);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
