d7148e8a8510baa0974b1a7606330aea7cbc8ac9
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-13 08:03:42 -0800

    Finish greet customer

--- objectPractice.js
+++ objectPractice.js
@@ -2054,13 +2054,68 @@
 
 // console.log(convertObjectToArray(input));
 
-
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// Complete a function called greetCustomers.
+
+// Given a name, greetCustomers returns a greeting based on how many times that customer has visited the restaurant. Please refer to the customerData object.
+
+// The greeting should be different, depending on the name on their reservation.
+
+// Case 1 - Unknown customer ( Name is not present in customerData ):
+
+// var output = greetCustomer('Terrance');
+// console.log(output); // --> 'Welcome! Is this your first time?'
+// Case 2 - Customer who has visited only once ( 'visits' value is 1 ):
+
+// var output = greetCustomer('Joe');
+// console.log(output); // --> 'Welcome back, Joe! We're glad you liked us the first time!'
+// Case 3 - Repeat customer: ( 'visits' value is greater than 1 ):
+
+// var output = greetCustomer('Carol');
+// console.log(output); // --> 'Welcome back, Carol! So glad to see you again!'
+// Notes:
+
+// Your function should not alter the customerData object to update the number of visits.
+// Do not hardcode to the exact sample data. This is a BAD IDEA:
+// if (firstName === 'Joe') {
+//   // do something
+// }
 
 
+var customerData = {
+  'Joe': {
+    visits: 1
+  },
+  'Carol': {
+    visits: 2
+  },
+  'Howard': {
+    visits: 3,
+  },
+  'Carrie': {
+    visits: 4
+  }
+};
+
+function greetCustomer(firstName) {
+  var greeting = '';
+  for(var name in customerData){
+    for(var value in customerData[name]){
+    if(name === firstName && customerData[name][value] === 1){
+      greeting = 'Welcome back, ' + firstName + "! We're glad you liked us the first time!";
+    }
+    else if(name === firstName && customerData[name][value] >1){
+      greeting = 'Welcome back, ' + firstName + '! So glad to see you again!';
+    }else if (!(firstName in customerData)){
+      greeting = 'Welcome! Is this your first time?';
+    }
+   }  
+  }
+  return greeting;
+};
+
+console.log(greetCustomer(customerData));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
