7fe67c1a75d02efa5b6809be7570601a1c0fd74a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-04-11 00:18:17 -0700

    Finish each

--- HRStopWatch.js
+++ HRStopWatch.js
@@ -22,3 +22,20 @@ _.uniq = function(array, isSorted, iterator) {
 
   return unique;
 };
+
+// Call iterator(value, key, collection) for each element of collection.
+  // Accepts both arrays and objects.
+  //
+  // Note: _.each does not have a return value, but rather simply runs the
+  // iterator function over each item in the input collection.
+  _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
