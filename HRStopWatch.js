55be5ec6b21f702775e435aa98f9e9d1b02e1d49
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-04-10 18:18:49 -0700

    Finish _uniq

--- HRStopWatch.js
+++ HRStopWatch.js
@@ -5,3 +5,20 @@ function doStuff(arr) {
 
 console.log(doStuff(myArray));
 
+_.uniq = function(array, isSorted, iterator) {
+  var unique = [];
+  for (var i = 0; i < array.length; i ++) {
+    if (isSorted && iterator) {
+      var transformedEl = iterator(array[i]);
+      if (!unique.includes(transformedEl)) {
+        unique.push(transformedEl);
+      }
+    } else {
+      if (!unique.includes(array[i])) {
+        unique.push(array[i]);
+      }
+    }
+  }
+
+  return unique;
+};
